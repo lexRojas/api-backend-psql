bf57c487bac5f1538cf3d755a520f24abad15707
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-08-30 20:11:44 -0600

    presupuesto filtrado

--- routes/route_presupuesto.py
+++ routes/route_presupuesto.py
@@ -1,7 +1,7 @@
 
 from fastapi import APIRouter
-from fastapi import Depends,HTTPException
-from sqlalchemy import select,exc
+from fastapi import Depends
+from sqlalchemy import select
 
 from config.db import AsyncSession, get_db
 from models.modelos import tb_presupuesto
@@ -10,8 +10,10 @@ from schemas.schemas import tb_presupuesto_BM
 route_presupuesto = APIRouter()
 
 @route_presupuesto.get("/tb_presupuesto")
-async def get_presupuesto(db: AsyncSession = Depends(get_db)) :
-    results = await db.execute(select(tb_presupuesto).order_by(tb_presupuesto.fecha_cambio.desc()).limit(10))
+async def get_presupuesto(filtro='', db: AsyncSession = Depends(get_db)) :
+    search = "%{}%".format(filtro)
+    print(search)
+    results = await db.execute(select(tb_presupuesto).filter(tb_presupuesto.proyecto.like(search.upper())).order_by(tb_presupuesto.fecha_cambio.desc()).limit(10))
     records = results.scalars().all()
     return records
     
